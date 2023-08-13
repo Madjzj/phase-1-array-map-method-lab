39f0b197112437d9c14e4add59d890c47de13d6d
JakobJohnson <madjzj@gmail.com>
2023-08-13 11:22:08 -0700

    Completed Lab

--- index.js
+++ index.js
@@ -12,5 +12,25 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  const titleCasedTutotirals = tutorials.map(capitalizer)
+  return titleCasedTutotirals
+}
+function capitalizer(line){
+  let newStr = ""
+  let isSpace = true;
+  let x = 0;
+  while(x < line.length){
+    if(isSpace){
+      newStr += line[x].toUpperCase();
+      isSpace = false;
+    } else {
+      newStr += line[x];
+    }
+    if(line[x] === " "){
+      isSpace = true;
+    }
+    x++;
+  }
+  line = newStr;
+  return line;
 }
